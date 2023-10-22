47885564e96bd30b02050b40c686d6b0f0a3ab4b
Matias <maticastrook@gmail.com>
2023-10-22 20:19:43 -0300

    first commit

---
+++ src/routes/product.routes.ts
@@ -0,0 +1,22 @@
+
+import express from "express";
+import * as ProductController from "../controllers/product.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { validateAddProduct, validateProductId } from "../validators/product.validator";
+
+const productRouter = express.Router();
+
+//List Products
+productRouter.get("/",ProductController.listProducts);
+
+//Get product by ID
+productRouter.get("/:id",validateProductId, ProductController.getProductById);
+
+//Upload product
+productRouter.post("/",authMiddleware,validateAddProduct, ProductController.addProduct);
+
+//Delete product by ID
+productRouter.delete("/:id", authMiddleware,validateProductId, ProductController.deleteProduct);
+
+export default productRouter;
+
